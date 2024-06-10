d7eb4e4e61523055b9c558796ea8181dceb8b4bb
lucKulke <lucaskulke98@gmail.com>
2024-06-10 23:16:28 +0200

    more testing

--- backend/cli_tool/test.py
+++ backend/cli_tool/test.py
@@ -6,20 +6,47 @@ file = open("output_json/test.json", "r")
 dict_obj = json.loads(file.read())
 
 
-class Section:
-    def __init__(self, ids: list, name: str) -> None:
-        self.ids = ids
-        self.name = name
+class Option:
+    def __init__(self, id: str, value: str, form_input: str) -> None:
+        self.id = id
+        self.value = value
+        self.form_input = form_input
 
     def __str__(self) -> str:
-        return f"name: {self.name} ids: {self.ids}"
+        return f"{self.value}"
+
+    def __repr__(self) -> str:
+        return f"{self.value}"
+
+
+class Section:
+    def __init__(self, ids: list, name: str, options: dict) -> None:
+        self.ids = ids
+        self.name = name
+        self.options = self.get_options(options)
+
+    def get_options(self, options: dict):
+        option_list = []
+        for key, value in options.items():
+            option_list.append(
+                Option(id=key, value=value["name"], form_input=value["form_input"])
+            )
+
+        return option_list
+
+    def __str__(self) -> str:
+        return f"name: {self.name} ids: {self.ids} options: {self.options}"
+
+
+class LLM:
+    pass
 
 
 sections = []
 
 for key, value in dict_obj.items():
 
-    sections.append(Section(ids=value.keys(), name=key))
+    sections.append(Section(ids=[*value], name=key, options=value))
 
 for section in sections:
     print(section)
