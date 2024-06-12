d6e6d9bed575a4ce051d1f6aa0dd2bf07242e15d
lucKulke <lucaskulke98@gmail.com>
2024-06-12 21:48:08 +0200

    basic flow works now

--- backend/cli_tool/test.py
+++ backend/cli_tool/test.py
@@ -1,53 +1,64 @@
 import os
 import json
-from library.components import Section
-from library.ais import LLM, IntentRecognition
+
+from library.llms import LLM
+from library.intent_recognition import IntentRecognition
 from dotenv import load_dotenv
 
 load_dotenv()
 file = open("output_json/test.json", "r")
 API_KEY = os.getenv("CHATGPT_API_KEY")
 FORM_FILLING_DATA = json.loads(file.read())
-TEST_MESSAGE = "Ich habe den Reifendruck aller reifen geprüft und er ist nicht in ordnung."
-
+INITIAL_INTENT_OPTIONS = {
+    "0": "The user indicates that he has completed a task or want to log some information.",
+    "1": "The user indicates that he want to make or have made a correction to a previous statement or task.",
+}
 chatgpt = LLM(llm_type="chatgpt", model="gpt-3.5-turbo", api_key=API_KEY)
 llm_config_file = open("llm_prompt_config.json", "r")
 llm_prompt_config = json.loads(llm_config_file.read())
 intent_recognizer = IntentRecognition(llm=chatgpt, config=llm_prompt_config)
 
-data = {
-    "0" : "He has completed a task.",
-    "1": "He wants to make a correction."
-}
+filled_form_fields = {}
 
-response = intent_recognizer.operation_intent(text=TEST_MESSAGE, data=data)
-result = response.choices[0].message.content
+while True:
+    text_message = input("enter text:")
+    if text_message == "exit":
+        break
 
-if (True):
-    print(data[result])
+    response_id = intent_recognizer.inital_intent(
+        data=INITIAL_INTENT_OPTIONS, text=text_message
+    )
 
-    data = {}
+    if response_id == "0":
+        task_name, status_id = intent_recognizer.process(
+            data=FORM_FILLING_DATA, text=text_message
+        )
+        if task_name in filled_form_fields:
+            filled_form_fields[task_name][status_id] = FORM_FILLING_DATA[task_name][
+                status_id
+            ]
+        else:
+            filled_form_fields[task_name] = {
+                status_id: FORM_FILLING_DATA[task_name][status_id]
+            }
 
-    task_desc_id = 0
-    for task_desc, value in FORM_FILLING_DATA.items():
-        data[str(task_desc_id)] = task_desc
-        task_desc_id += 1
-    
-    response = intent_recognizer.task_desc_match(text=TEST_MESSAGE, data=data)
-    result = response.choices[0].message.content
-    print(data[result])
-    task_desc_result = data[result]
-    option_data = {}
-    for operation_id, value in FORM_FILLING_DATA[task_desc_result].items():
-        option_data[operation_id] = value["name"]
-        
-    data = {}
-    data[task_desc_result] = option_data
-    response = intent_recognizer.option_match(text=TEST_MESSAGE, data=data)
-    result = response.choices[0].message.content
-    print(option_data[result])
-    
+        if FORM_FILLING_DATA[task_name][status_id]["form_input"] == "text":
+            substring = intent_recognizer.find_substring(
+                substring_description=FORM_FILLING_DATA[task_name][status_id]["name"],
+                text=text_message,
+            )
+            filled_form_fields[task_name][status_id]["text_value"] = substring
 
+    elif response_id == "1":
+        task_name, status_id = intent_recognizer.process(
+            data=FORM_FILLING_DATA, text=text_message
+        )
+        if task_name in filled_form_fields:
+            filled_form_fields[task_name] = {}
+            filled_form_fields[task_name] = {
+                status_id: FORM_FILLING_DATA[task_name][status_id]
+            }
+        else:
+            print("no correction possible")
 
-elif(result == "1"):
-    print(data[result])
+    print(filled_form_fields)
