4f54f21de517e84b1b136709a441e59bc60b74c6
lucKulke <lucaskulke98@gmail.com>
2024-06-12 16:33:37 +0200

    flow test

--- backend/cli_tool/test.py
+++ backend/cli_tool/test.py
@@ -7,38 +7,47 @@ from dotenv import load_dotenv
 load_dotenv()
 file = open("output_json/test.json", "r")
 API_KEY = os.getenv("CHATGPT_API_KEY")
-dict_obj = json.loads(file.read())
-TEST_MESSAGE = "Korrigiere, der Reifendruck ist doch nicht in ordnung."
-
-sections = []
-
-for key, value in dict_obj.items():
-
-    sections.append(Section(ids=[*value], name=key, options=value))
-
-
-data = []
-id = 0
-for section in sections:
-    data.append(f"{id}: {section.name}")
-    id += 1
-
-print(data)
-
+FORM_FILLING_DATA = json.loads(file.read())
+TEST_MESSAGE = "Ich habe den Reifendruck aller reifen geprüft und er ist nicht in ordnung."
 
 chatgpt = LLM(llm_type="chatgpt", model="gpt-3.5-turbo", api_key=API_KEY)
 llm_config_file = open("llm_prompt_config.json", "r")
 llm_prompt_config = json.loads(llm_config_file.read())
 intent_recognizer = IntentRecognition(llm=chatgpt, config=llm_prompt_config)
 
-data = [
-    {0: "He has completed a task and wants to log it"},
-    {1: "He corrects his statement"},
-]
+data = {
+    "0" : "He has completed a task.",
+    "1": "He wants to make a correction."
+}
+
 response = intent_recognizer.operation_intent(text=TEST_MESSAGE, data=data)
 result = response.choices[0].message.content
 
-if result == "1":
-    pass
-elif result == "0":
-    pass
+if (True):
+    print(data[result])
+
+    data = {}
+
+    task_desc_id = 0
+    for task_desc, value in FORM_FILLING_DATA.items():
+        data[str(task_desc_id)] = task_desc
+        task_desc_id += 1
+    
+    response = intent_recognizer.task_desc_match(text=TEST_MESSAGE, data=data)
+    result = response.choices[0].message.content
+    print(data[result])
+    task_desc_result = data[result]
+    option_data = {}
+    for operation_id, value in FORM_FILLING_DATA[task_desc_result].items():
+        option_data[operation_id] = value["name"]
+        
+    data = {}
+    data[task_desc_result] = option_data
+    response = intent_recognizer.option_match(text=TEST_MESSAGE, data=data)
+    result = response.choices[0].message.content
+    print(option_data[result])
+    
+
+
+elif(result == "1"):
+    print(data[result])
