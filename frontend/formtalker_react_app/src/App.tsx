fc493769d8b5eb78fa5a29283e8a8b57d56e3535
lucKulke <lucaskulke98@gmail.com>
2024-06-30 18:09:27 +0200

    successfully added shadcn to react app

--- frontend/formtalker_react_app/src/App.tsx
+++ frontend/formtalker_react_app/src/App.tsx
@@ -2,9 +2,14 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { Button } from "@/components/ui/button";
 
 function App() {
-  return <>Hello World</>;
+  return (
+    <div>
+      <Button>test me</Button>
+    </div>
+  );
 }
 
 export default App;
