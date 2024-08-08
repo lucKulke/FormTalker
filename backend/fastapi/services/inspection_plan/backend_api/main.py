b64d64789af697fc982e40064e493eb7e8ce1e60
lucKulke <lucaskulke98@gmail.com>
2024-08-08 15:29:43 +0200

    testing on backend server

--- backend/fastapi/services/inspection_plan/backend_api/main.py
+++ backend/fastapi/services/inspection_plan/backend_api/main.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI
+import os
+from supabase import create_client, Client
+from dotenv import load_dotenv
+
+load_dotenv()
+url: str = os.environ.get("SUPABASE_URL")
+key: str = os.environ.get("SUPABASE_KEY")
+supabase: Client = create_client(url, key)
 
 app = FastAPI()
 
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+
+    response = supabase.table("formsis").select("*").execute()
+    print(response)
+    return response
+
+
+# app.include_router(security_routes.router)
